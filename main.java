fab43a0a0f45dae1072efff876312fc1dbf0e027
taoge <tg367@cornell.edu>
2020-03-13 23:57:59 -0400

    first vision

---
+++ main.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+/**
+ * main
+ */
+public class main {
+
+    public static void main(String[] args) {
+        if(args.length<2) {
+            System.out.println("Invalid input");
+            return;
+        }
+        String source=args[0];
+        String target=args[1];
+        if(source.length()!=target.length()){
+            System.out.println("Length not equal");
+            return;
+        }
+        System.out.println(helper(source, target)?"true":"false");
+        
+    }
+    public static boolean helper(String source, String target) {
+        HashMap<Character,Character> map=new HashMap<>();
+        for(int i=0;i<source.length();i++){
+            char c=source.charAt(i);
+            if(!map.containsKey(c)){
+                map.put(c, target.charAt(i));
+            }else{
+                if (map.get(c)!=target.charAt(i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
